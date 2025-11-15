652f7ccc9ee792a2fb7e8e5247a5de7371694c80
unknown <pooja.v@sharajman.com>
2025-11-15 23:27:05 +0530

    api onboarding and check module file

---
+++ GERP_Modules_Automation/api_onboarding_through.py
@@ -0,0 +1,100 @@
+import requests
+import json
+from Setup import *
+
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiaWthc2guc2Fob29Ac2hhcmFqbWFuLmNvbSIsImxvZ2luX2lkIjoyNiwidXNlcl9pZCI6MzEsInVzZXJfdHlwZSI6Ik8mTSBURUFNIiwiZXhwIjoxNzYzMjU5MDc0fQ.-HFPeXhrC3mUxKqr_gQ79OsAGn73-VTHyjxTaCRCeew"
+base_url = "https://app.release.gensomerp.com"
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+driver, wait, actions = setup_driver()
+login(driver, base_url)
+
+file_path = "C:\Automation\GERP\project_details.xlsx"
+df = pd.read_excel(file_path, "demo")
+data_list = df.to_dict(orient="records")
+
+for i in range(1, 2):    
+    for item in data_list:
+        project_name = item.get("project_name")
+        # print(f"DEMO-{project_name}-{i:03d}")
+        do = i + 100
+        plant_payload = {
+            "plant_code": f"demo-{project_name}-{i:03d}", #need to fetch from excel sheet
+            "plant_name": f"DEMO-{project_name}-{i:03d}", #need to fetch from excel sheet
+            "short_name": f"{project_name}{i:03d}", #need to fetch from excel sheet
+            "site_address": "Testing address", #need to fetch from the excel sheet
+            "state_id": "3", #need to fetch from the excel sheet
+            "latitude": 52.96, #need to fetch from the excel sheet
+            "longitude": 52.36, #need to fetch from the excel sheet
+            "cluster_id": "3",
+            "company_id": "21",
+            "domain": "SOLAR_MANAGEMENT",
+            "sub_domain": "SOLAR_POWER_STORAGE",
+            "technology_type": "MONOCRYSTALLINE",
+            "installation_type": "GROUND MOUNT",
+            "mounting_type": "FIXED TILT",
+            "tilt": 30.5,
+            "dc_capacity": 1000, #need to fetch from the excel sheet
+            "ac_capacity": 1500, #need to fetch from the excel sheet
+            "commisioning_date": "2025-11-12",
+            "warehouse_id": "97",
+            "tariff": 100,
+            "data_frequency": "5", #need to fetch from excel sheet
+            "is_wms_installed": True,
+            "is_smb_installed": True,
+            "start_time": "07:00",
+            "end_time": "19:30",
+            "display_order": do #need to fetch from the excel sheet
+        }
+
+
+        create_plant_url = f"{base_url}/api/add_basic_plant_details"
+
+        try:
+            response = requests.post(create_plant_url, headers=headers, data=json.dumps(plant_payload))
+            print("Status Code:", response.status_code)
+
+            if response.status_code == 201:
+                response_data = response.json()
+                project_id = response_data.get("data", {}).get("plant_id")
+
+                if not project_id:
+                    print("Unable to fetch the project id from the response.")
+                else:
+                    print(f"Project ID: {project_id}")
+
+                    try:
+                        site_user_url = f"{base_url}/api/link_user_to_project/{project_id}"
+                        site_person_payload = {
+                            "user_id": 31,
+                            "role_type": "SITE_TECH"
+                        }
+
+                        link_response = requests.post(site_user_url, headers=headers, data=json.dumps(site_person_payload))
+                        print("\nUser Status Code:", link_response.status_code)
+
+                        if link_response.status_code == 200:
+                            print(f"User linked successfully to project ID {project_id}")
+                        else:
+                            print(f"Failed to link user: {link_response.text}")
+
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error while linking user to project: {e}")
+
+            else:
+                print(f"Project creation failed: {response.text}")
+
+        except requests.exceptions.Timeout:
+            print("Request timed out. Please check your network connection.")
+        except requests.exceptions.ConnectionError:
+            print("Network connection error. Please verify internet connectivity.")
+        except requests.exceptions.RequestException as e:
+            print(f"Unexpected error while creating project: {e}")
+
+
+
+    
+
